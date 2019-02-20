348d7f79b199f4b232b571494e0c449290f4acb2
SharonC <sharon.s.c.chien@gmail.com>
2019-02-20 01:50:06 -0500

    yay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var tiana = {
     answers: ["Aurora", "Belle", "Jasmine", "Tiana"],
     correctAns: "Tiana",
     answerLine: "Tiana is the only princess who is left-handed other than the honorary-royal, Mulan.",
-    video: "",
+    video: "./assets/videos/tiana.mp4",
 };
 
 var merida = {
@@ -12,7 +12,7 @@ var merida = {
     answers: ["Ariel", "Snow White", "Merida", "Rapunzel"],
     correctAns: "Merida",
     answerLine: "Merida is the only princess to have brothers",
-    video: "",
+    video: "./assets/videos/merida.mp4",
 };
 
 var pocahontas = {
@@ -20,7 +20,7 @@ var pocahontas = {
     answers: ["Pocahontas", "Tiana", "Merida", "Mulan"],
     correctAns: "Pocahontas",
     answerLine: "Pocahontas was the only princess based on a real person. Mulan was based on a poem.",
-    video: "",
+    video: "./assets/videos/pocahontas.mp4",
 };
 
 var cinderella = {
@@ -28,7 +28,7 @@ var cinderella = {
     answers: ["Cinderella", "Ariel", "Pocahontas", "Belle"],
     correctAns: "Cinderella",
     answerLine: "In earlier drafts of the film, Cinderella originally had a pet turtle named Clarissa.",
-    video: "",
+    video: "./assets/videos/cinderella.mp4",
 };
 
 var moana = {
@@ -36,7 +36,7 @@ var moana = {
     answers: ["Anna", "Moana", "Shuri", "Gamora"],
     correctAns: "Moana",
     answerLine: "Moana is almost a full fledge princess!",
-    video: "",
+    video: "./assets/videos/moana.mp4",
 };
 
 //Setting up Trivia question list as an array of objects
@@ -46,41 +46,31 @@ var triviaList = [tiana, merida, pocahontas, cinderella, moana];
 $(document).ready(function (){
     $("#start").show();
     $("#deck").empty();
-    qTime = 10;
-    aTime = 4;
+    $(".video").hide();
+    qTime = 30;
     next = 0;
     
 });
 
 //some required time/tracking variables
-var qTime, aTime, next;
+var qTime, next, qTimer, aTimer;
 
 // timer for question count down
 function qCD(){
-    if (qTime > 0){
-        var timer = setInterval(function() {
-            $("#timer").text("Time remaining: " + qTime--  + " seconds.");
-            displayQ();
-        }, 1000);
-    } else {
-        aTime = 4;
-        aCD();
-        clearInterval(timer);
-    };
+    clearTimeout(aTimer);
+    qTimer = setInterval(countDown, 1000);
+    displayQ();
 };
 
 //timer for answer count down
 function aCD(){
-    var timer = setInterval(function() {
+    if (next === triviaList.length - 1){
+        $("#timer").empty().hide();
+        $("#deck").empty();
+        $("<h5>You have finished the game!</h5><br>").addClass("card-title").appendTo($("#deck"));
+    } else {
+        clearInterval(qTimer);
         displayA();
-        $("#timer").empty();
-        aTime--;
-    }, 1000);
-    if (aTime === 0){
-        next++
-        qTime = 10;
-        qCD();
-        clearInterval(timer);
     };
 };
 
@@ -91,30 +81,58 @@ $("#start").on("click", function(){
 });
 
 
+//count down
+function countDown(){
+    qTime--
+    $("#timer").text("Time remaining: " + qTime + " seconds.");
+    if (qTime === 0){
+        clearInterval(qTimer);
+        $("#timer").empty().hide();
+        $("#deck").empty();
+        $("<h5>Time's up!</h5>").addClass("card-title").appendTo($("#deck"));
+        aCD();
+    }
+
+};
+
 //function to display each quesiton
 function displayQ(){
+    $(".video").hide();
     $("#deck").empty();
+    $(".video").hide();
+    $("#playVideo").attr("src", "");
+    $("#timer").text("Time remaining: " + qTime + " seconds.");
     $("<h5>" + triviaList[next].question + "</h5>").addClass("card-title").appendTo($("#deck"));
     for (var i = 0; i < 4; i++){
         if (triviaList[next].answers[i] === triviaList[next].correctAns){
-            $("<button>" + triviaList[next].answers[i] + "</button>").addClass("mx-2 btn btn-info correct").appendTo($("#deck"));
+            $("<button>" + triviaList[next].answers[i] + "</button>").addClass("answer mx-2 my-1 btn correct").appendTo($("#deck"));
         } else {
-        $("<button>" + triviaList[next].answers[i] + "</button>").addClass("mx-2 btn btn-info").appendTo($("#deck"));
+        $("<button>" + triviaList[next].answers[i] + "</button>").addClass("answer mx-2 my-1 btn").appendTo($("#deck"));
         };
 
     };
 };
 
-//check answer correctly
-
-
 //function to display answers
 function displayA(){
-    $("#deck").empty();
-    if (next < triviaList.length){
-        $("<h5>" + triviaList[next].answerLine + "</h5>").addClass("card-title").appendTo($("#deck"));
-    }else {
-        $("<h5>You have finished the game!</h5>").addClass("card-title").appendTo($("#deck"));
-    }
+    $("<p>" + triviaList[next].answerLine + "</p>").appendTo($("#deck"));
+    $(".video").show();
+    $("#playVideo").attr("src", triviaList[next].video);
+    next++;
+    qTime = 30;
+    aTimer = setTimeout(qCD, 10000);
 };
 
+//function to answer question
+$("body").on("click", ".answer", function(){
+    $("#timer").empty().hide();
+    $("#deck").empty();
+    clearInterval(qTimer);
+    if($(this).hasClass("correct")){
+        $("<h5> You are corret!</h5>").addClass("card-title").appendTo($("#deck"));
+        displayA();        
+    } else {
+        $("<h5>That is incorrect...</h5>").addClass("card-title").appendTo($("#deck"));
+        displayA();
+    };
+});
